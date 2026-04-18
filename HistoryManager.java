a007c41dd081354bd8af42c2ac42a8c856a2622a
左足の小指の爪 <yuhi226.k@gmail.com>
2026-04-18 22:49:08 +0900

    Delete HistoryManager.java

--- HistoryManager.java
+++
@@ -1,8 +0,0 @@
-private void saveHistory() {
-    SharedPreferences prefs = getSharedPreferences("url_history", MODE_PRIVATE);
-    JSONArray arr = new JSONArray();
-
-    for (String url : history) arr.put(url);
-
-    prefs.edit().putString("history", arr.toString()).apply();
-}
